1401ae317f108814e8adb2fda2a840d2a68a0704
as223my <as223my@student.lnu.se>
2014-11-27 21:56:46 +0100

    update

--- GuessSecretNumber/Controllers/SecretNumberController.cs
+++ GuessSecretNumber/Controllers/SecretNumberController.cs
@@ -8,90 +8,46 @@ using GuessSecretNumber.Models;
 
 namespace GuessSecretNumber.Controllers
 {
+
     public class SecretNumberController : Controller
     {
+        private SecretNumber SecretNumber
+        {
+            get { return Session["sn"] as SecretNumber ?? (SecretNumber)(Session["sn"] = new SecretNumber()); }
+        }
+
         public ActionResult Index()
         {
-            // Objekten i session tas bort så nytt spel kan börja. 
-            if (Session["objekt"] != null)
+            if (Session["sn"] != null)
             {
-                Session.Clear();
+                Session.Clear(); 
             }
-
-            // Sätter en session, vars uppgift är att kolla om tiden gått ut eller ej. 
-            Session["Game"] = "Game"; 
-            return View(); 
+       
+            return View(SecretNumber);
         }
 
         [HttpPost, ActionName("Index"), ValidateAntiForgeryToken]
-        public ActionResult Index_POST(SecretNumber model, FormCollection collection)
+        public ActionResult Index_POST(FormCollection collection)
         {
-            if (Session["Game"] == "Game")
+            
+            if (Session.IsNewSession)
             {
-                // Testar om input talet i formuläret är godkänt enlig villkor, försökte ha båda i samma men fick ingen rätsida på det. 
-                if (TryUpdateModel(model, new[] { "NewNumber" }, collection) && model.NewNumber > 100)
-                {
-                    ModelState.AddModelError("NewNumber", "* Du kan inte gissa på ett tal större än 100!");
-                }
-                if (TryUpdateModel(model, new[] { "NewNumber" }, collection) && model.NewNumber < 1)
-                {
-                    ModelState.AddModelError("newnumber", "* Du kan inte gissa på ett tal mindre än 1!");
-                }
-
-                List<SecretNumber> list = new List<SecretNumber>();
-
-                // En ny spelomgång börjar om Session["objekt"] är null.
-                if (Session["objekt"] == null)
-                {
-                    // Om inputen i formuläret var giltligt, kan en gissning ske. Annars "stannar" man kvar i Index vyn och får ett felmeddelande. 
-                    if (ModelState.IsValid)
-                    {
-                        Outcome outcome = model.MakeGuess(model.NewNumber);
-                        if (outcome == Outcome.Right)
-                        {
-                            return View("RightGuess", model);
-                        }
-
-                        // Lägger till vårat första objekt till Session["objekt"], så att vi kan bygga på listan GuessedNumber (_guessedNumbers) i modellen, och hålla koll på det hemliga talet.
-                        list.Add(model);
-                        Session["objekt"] = list;
-
-                        return View("Guesses", model);
-                    }
-
-                    return View("Index"); 
-                }
-
-                // Om en gissning redan har skett sedan innan, dvs ett spel har påbörjats. 
-
-                list = (List<SecretNumber>)Session["objekt"];
-
-                // Om ett fel sker i spelomgången returneras det första objektet till vyn, annars byggs listan på med nästa gissning. 
-                SecretNumber _firstmodel = model; // tilldelades model tillfälligt då jag fick klagomål från return annars. 
-
-                foreach (SecretNumber firstModel in (List<SecretNumber>)Session["objekt"])
-                {
-                    _firstmodel = firstModel;
-
-                    if (ModelState.IsValid)
-                    {
-                        // Lägger till det nya objektets tal (model) i det första sparade objektets lista med GuessedNumbers. 
-                        Outcome outcome = firstModel.MakeGuess(model.NewNumber);
-                        if (outcome == Outcome.Right)
-                        {
-                            return View("RightGuess", firstModel);
-                        }
-
-                        return View("Guesses", firstModel);
-                    }
-                }
-
-                return View("Guesses", _firstmodel);
-
-            }else{
-                Session.Clear();
                 return View("error");
             }
+
+            if (TryUpdateModel(SecretNumber, new[] { "NewNumber" }, collection))
+            {
+                Outcome outcome = SecretNumber.MakeGuess(SecretNumber.NewNumber);
+                if (outcome == Outcome.Right)
+                {
+                    return View("RightGuess", SecretNumber);
+                }
+
+                return View("Guesses", SecretNumber);
+            }
+
+            return View("Index");
+
         }
     }
 }
--- GuessSecretNumber/Models/SecretNumber.cs
+++ GuessSecretNumber/Models/SecretNumber.cs
@@ -15,7 +15,7 @@ namespace GuessSecretNumber.Models
         private GuessedNumber _lastGuessedNumber; // Innehåller det senaste gissade numret. 
         private List<GuessedNumber> _guessedNumbers; // Lista med alla gissade nummer.
 
-
+        [Range(1, 100, ErrorMessage = "* Talet måste vara mellan 1 till 100!")]
         [DisplayName("Gissa på ett tal mellan 1 och 100: "), Required(ErrorMessage = "* Du måste skicka in ett tal mellan 1 till 100!")]
         public int NewNumber { get; set; }
         public SecretNumber()
@@ -55,7 +55,7 @@ namespace GuessSecretNumber.Models
         }
 
         // Lista (readOnly) som innehåller alla objekt gissade nummer samt Outcome. 
-        public IList<GuessedNumber> GuessedNumbers
+        public IReadOnlyList<GuessedNumber> GuessedNumbers
         {
             get
             {
